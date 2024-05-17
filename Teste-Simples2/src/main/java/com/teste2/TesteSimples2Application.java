a3b3fc3c91b42e2b45801f20d45ae952f1980ec2
Amandabgoes <amandabriganti15@hotmail.com>
2024-05-17 14:42:30 -0300

    teste2

---
+++ Teste-Simples2/src/main/java/com/teste2/TesteSimples2Application.java
@@ -0,0 +1,13 @@
+package com.teste2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSimples2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSimples2Application.class, args);
+	}
+
+}
